55ffd69588a4026ce93cdeefc61fcb3a23f1965d
Grégor Sternat <gregsternat@gmail.com>
2023-07-13 15:44:11 +0200

    init

---
+++ ft_print_numbers.c
@@ -0,0 +1,14 @@
+int ft_putchar (char c);
+
+void ft_print_numbers()
+{
+    char a;
+    a = '0';
+
+    while (a <= '9')
+    {
+        ft_putchar(a);
+        a++;
+    }
+
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "test.c"
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main()
+{
+    ft_print_comb();
+	return 0;
+}
