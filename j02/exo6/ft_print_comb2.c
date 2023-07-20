b6afd33ebf2cb195ab3d9ad8500d91b11560c93f
Grégor Sternat <107589545+Bouboubz@users.noreply.github.com>
2023-07-20 23:00:44 +0000

    v1

--- j02/exo6/ft_print_comb2.c
+++ j02/exo6/ft_print_comb2.c
@@ -1,54 +1,6 @@
-int ft_putchar(char c);
-void print_numbers(char a, char b, char c, char d);
-void print_space_comma();
+#include "ft_print_comb2.h"
 
-void ft_print_comb2()
+void ft_print_comb2(void)
 {
-    char a;
-    char b;
-    char c;
-    char d;
-    a = '0';
-    while (!(a == '9' && b == '8' && c == '9' && d == '9'))
-    {
-        if (d == ':')
-        {
-             c = '0';
-             b++;
-             while(b >= c)
-             {
-                c++;
-                if (b == '9')
-                {
-                    b = '0';
-                    a++;
-                    while (a >=b)
-                    {
-                        b++;
-                    }
-                }
-             }
-        }
-        print_numbers(a, b, c);
-        d++;
-        if(a != '7')
-        {
-            print_space();
-        }
-    }
-}
-
-void print_numbers(char a, char b, char c, char d)
-{
-    ft_putchar(a);
-    ft_putchar(b);
-    ft_putchar(' ');
-    ft_putchar(c);
-    ft_putchar(d);
-}
-
-void print_space_comma()
-{
-    ft_putchar(',');
-    ft_putchar(' ');
+    
 }
