1935818e2219d37f8570f0ff07430aac8d3eb1e2
Grégor Sternat <107589545+Bouboubz@users.noreply.github.com>
2023-07-31 22:51:48 +0000

    v1

--- Day 04 - Pointers/Task 04 - my_evil_str/my_evil_str.c
+++ Day 04 - Pointers/Task 04 - my_evil_str/my_evil_str.c
@@ -3,12 +3,18 @@
 char *my_evil_str(char *str)
 {
     int counter = 0;
+    int i = 0;
     while (str[counter] != '\0') {
         counter++;
     }
     while (counter != -1) {
-        char a = str[counter];
-        my_putchar(a);
+        char d = str[i];
+        str[counter] = d;
+        i++;
         counter--;
     }
+    /*for (int b = 0 ; str[b] != '\0' ; b++) {
+        char a = str[b];
+        my_putchar(a);
+    } */
 }
