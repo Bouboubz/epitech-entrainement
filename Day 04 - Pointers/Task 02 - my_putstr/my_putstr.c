b21b171e63108f70ca7964ef96f312acea0c180a
Grégor Sternat <107589545+Bouboubz@users.noreply.github.com>
2023-08-18 13:26:50 +0000

    v2

--- Day 04 - Pointers/Task 02 - my_putstr/my_putstr.c
+++ Day 04 - Pointers/Task 02 - my_putstr/my_putstr.c
@@ -4,7 +4,7 @@ int my_putstr(char const *str)
 {
 // We create a loop or an int that will increase and that we will use to write each letter of the word that the variable contains
     for(int counter = 0 ; str[counter] != '\0' ; counter ++ ) {
-        char letter = str[counter];
+        /* char letter = str[counter]; */
         my_putchar(letter);
     }
 }
